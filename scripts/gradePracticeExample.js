a326a968a77cf851826736ed6ffcf29fe1830e16
Azmin-Tahmina <123420809+Azmin-Tahmina@users.noreply.github.com>
2024-01-15 12:37:20 -0600

    test

--- scripts/gradePracticeExample.js
+++ scripts/gradePracticeExample.js
@@ -2,5 +2,34 @@
 
 let gradeInput = window.prompt("Enter valid grade(1-100)", "")
 
-window.alert(gradeInput)
+//window.alert(gradeInput)
+
+let outputVariable = ""
+let grade;
+let numberOfGrades = 0
+let totalNumber = 0
+let displayAlert = ""
+
+while( gradeInput !== null)
+{
+    grade = parseFloat(gradeInput)
+
+    if( grade < 0 || grade > 100 || Number.isNaN(grade))
+    {
+        window.prompt ("You entered invalid grade!Enter valid grade(1-100)")
+    }
+    else
+    {
+        numberOfGrades ++;
+        totalNumber += grade
+        displayAlert += "Numbers entered" + numberOfGrades + "\n"
+        displayAlert += "Total Number " + totalNumber + "\n"
+
+        window.alert(displayAlert)
+
+    }
+    
+    gradeInput = window.prompt("Enter valid grade(1-100)", "")
+}
+
 
