6fcb59cfd72a1771868b7055f9ad9feb275b694a
Azmin-Tahmina <123420809+Azmin-Tahmina@users.noreply.github.com>
2024-01-15 12:05:17 -0600

    2014-01-15

---
+++ scripts/gradePracticeExample.js
@@ -0,0 +1,6 @@
+"use strict"
+
+let gradeInput = window.prompt("Enter valid grade(1-100)", "")
+
+window.alert(gradeInput)
+
--- scripts/loopExample.js
+++ scripts/loopExample.js
@@ -2,18 +2,54 @@
 
 let outputString = ""
 
-for(let counter=10; counter>0; counter--)
+let upperRange = window.prompt("Enter value: ","10")
+
+upperRange = parseInt(upperRange)
+
+// for(let counter=10; counter>0; counter--)
+// {
+//     if( counter == 5)
+//     {
+//         outputString += " Five <br />"
+//     }
+//     else
+//     {
+//         outputString += counter+ "<br />"
+//     }
+// }
+
+let ouputValue = ""
+
+for(let counter=upperRange; counter>0; counter--)
 {
-    if( counter == 5)
-    {
-        outputString += " Five <br />"
-    }
-    else
-    {
-        outputString += counter+ "<br />"
-    }
+
+   switch(counter)
+   {
+        case 5:
+            ouputValue += "Five <br />"
+            break;
+        case 4:
+            ouputValue += "Four <br />"
+            break;
+        case 3:
+            ouputValue += "Three <br />"
+            break;
+        case 2:
+            ouputValue += "two <br />"
+            break;
+        case 1:
+            ouputValue += "one <br />"
+            break;
+        default:
+            ouputValue += counter + "<br />"
+            break;    
+                    
+   }
+
 }
 
-outputString += "<strong>Blastoff!</strong>";
+ouputValue += "<strong>Blastoff!</strong>";
 
-document.getElementById("output").innerHTML = outputString
+const outputArea = document.getElementById("output");
+// document.getElementById("output").innerHTML = ouputValue
+outputArea.innerHTML = ouputValue
